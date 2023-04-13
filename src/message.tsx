02c8d9d8a3c20f4bb3e584667c017458e5929be1
Pratik <pendurkarpratik04@gmail.com>
2023-04-13 23:25:26 +0530

    First react app

---
+++ src/message.tsx
@@ -0,0 +1,10 @@
+// PASCAL CASING, CAPITAL FIRST LETTER
+function Message() {
+  // const name = 'Pratik';
+  if (name) {
+    return <h1>Hello Pratik</h1>; // jsx javascript XML
+  }
+  return <h1>Hey There</h1>;
+}
+
+export default Message;
