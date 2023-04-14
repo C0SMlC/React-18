d4885388ec23991af7cca8fe6e34e149d3f809a5
Pratik <pendurkarpratik04@gmail.com>
2023-04-14 11:40:30 +0530

    Rendering List

--- src/message.tsx
+++ src/message.tsx
@@ -3,14 +3,31 @@
 import { Fragment } from 'react';
 // Can use fragment instead of <> </>
 function Message() {
+  const items = [
+    'New York',
+    'Delhi',
+    'Mumbai',
+    'San Francisco',
+    'Tokyo',
+    'Sydeny',
+  ];
+
   return (
     <>
       <h1>The List of greeting message</h1>
       <ul className="unorderedList">
-        <li className="item">Hello</li>
-        <li className="item">Hii</li>
-        <li className="item">Good Morning</li>
-        <li className="item">Good Evening</li>
+        {items.map((item) => (
+          <li key={item} className="item">
+            {item}
+          </li>
+          // Eahc item should have unique in order to react to identufy the item
+          // runtime.development.js:87  Warning: Each child in a list should have a unique "key" prop.
+          // Check the render method of `Message`. See https://reactjs.org/link/warning-keys for more information.
+          //     at li
+          //     at Message
+          //     at Greeting
+          // printWarning @ react-jsx-dev-ru
+        ))}
       </ul>
     </>
   );
